2de3d837fa7e93826d3c8d1ff35b519716634ebf
liao <liaomovie2@gmail.com>
2019-02-26 23:55:20 +0800

    fix figures/ problem: rm gitignore

--- pagedown.sh
+++ pagedown.sh
@@ -1,7 +1,6 @@
 mv out.html _bookdown_files/index.html
 [[ -e out.rds ]] && mv out.rds _bookdown_files/
-cp -r images/ css/  _bookdown_files/
-cp -r figures-ori/ _bookdown_files/figures
+cp -r figures/ images/ css/  _bookdown_files/
 
 [[ -d docs ]] && rm -r docs
 mv _bookdown_files docs
