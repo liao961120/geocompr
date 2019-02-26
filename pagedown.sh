8b4045d1f25df067b05a77ef98fdd3d786d084b9
liao <liaomovie2@gmail.com>
2019-02-26 23:23:21 +0800

    use pagedown

--- pagedown.sh
+++ pagedown.sh
@@ -1,6 +1,7 @@
 mv out.html _bookdown_files/index.html
 [[ -e out.rds ]] && mv out.rds _bookdown_files/
-cp -r figures/ images/ css/  _bookdown_files/
+cp -r images/ css/  _bookdown_files/
+cp -r figures-ori/ _bookdown_files/figures
 
 [[ -d docs ]] && rm -r docs
 mv _bookdown_files docs
